621ab4398082d1e5e506e41fd0b5ed488aa833c9
cnzhihua <cnzhihua@gmail.com>
2018-05-02 12:16:35 +0800

    修改environments数据库配置

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=gp',
             'username' => 'root',
-            'password' => '',
+            'password' => '123123',
             'charset' => 'utf8',
             'tablePrefix' => '',
         ],

55bc65900b84e4522dc8d0187aaa6453f6ad261a
cnzhihua <cnzhihua@gmail.com>
2018-05-10 13:32:32 +0800

    fix menu add bug

--- backend/views/admin-menu/create.php
+++ backend/views/admin-menu/create.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use backend\assets\JqueryMigrateAsset;
 
 /* @var $this yii\web\View */
 /* @var $model mdm\admin\models\Menu */
@@ -8,6 +9,9 @@ use yii\helpers\Html;
 $this->title = Yii::t('rbac-admin', 'Create Menu');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('rbac-admin', 'Menus'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+JqueryMigrateAsset::register($this);
+
 ?>
 <div class="menu-create">
 
